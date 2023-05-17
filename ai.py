d13e20749d7eb8948d4463acc8c954e2f7ab8e27
newYasser <mohammedyasser0110@gmail.com>
2023-05-18 02:21:43 +0300

    measure performance of minimax and alpha beta

--- ai.py
+++ ai.py
@@ -14,11 +14,67 @@ class AI:
     def __init__(self, level=1, player='o'):
         self.level = level
         self.player = player
+        self.node_count = 0
 
-    def minimax(self, board, maximizing, depth, alpha, beta):
+    def minimax(self, board, maximizing, depth):
+        self.node_count += 1
         # Base case
 
+        # print("board")
+        # board.print_board()
+        case = board.is_winning()
+        # print(case)
+        #   player 1 wins
+        if case == 'x':
+            return 1, None  # eval, move
 
+        # player 2 wins
+        if case == 'o':
+            return -1, None
+
+        # draw
+        elif board.is_full() or depth == 6:
+            return 0, None
+
+        if maximizing:
+            temp_board = Board()
+            max_eval = -2
+            best_move = None
+            empty_pos = board.get_empty_pos()
+
+            for (row, col) in empty_pos:
+                temp_board.board = board.copy_val()
+                temp_board.mark_pos(row, col, 'x')
+                evaluation = self.minimax(temp_board, False, depth + 1)[0]
+                if evaluation > max_eval:
+                    max_eval = evaluation
+                    best_move = (row, col)
+
+            return max_eval, best_move
+
+        elif not maximizing:
+            temp_board = Board()
+            min_eval = 2
+            best_move = None
+            empty_pos = board.get_empty_pos()
+            for (row, col) in empty_pos:
+                temp_board.board = board.copy_val()
+                # print(col)
+                # print(row)
+                temp_board.mark_pos(row, col, 'o')
+                evaluation = self.minimax(temp_board, True, depth + 1)[0]
+                if evaluation < min_eval:
+                    min_eval = evaluation
+                    best_move = (row, col)
+
+                # print(min_eval)
+                # print(best_move)
+
+            return min_eval, best_move
+
+    def minimax_alpha_beta(self, board, maximizing, depth, alpha, beta):
+        self.node_count += 1
+        # Base case
         #print("board")
         #board.print_board()
         case = board.is_winning()
@@ -46,7 +102,7 @@ class AI:
             for (row, col) in empty_pos:
                 temp_board.board = board.copy_val()
                 temp_board.mark_pos(row, col, 'x')
-                evaluation = self.minimax(temp_board, False, depth - 1, alpha- 1, beta)[0]
+                evaluation = self.minimax_alpha_beta(temp_board, False, depth - 1, alpha- 1, beta)[0]
                 if evaluation > max_eval:
                     max_eval = evaluation
                     best_move = (row, col)
@@ -67,7 +123,7 @@ class AI:
                 #print(col)
                 #print(row)
                 temp_board.mark_pos(row, col, 'o')
-                evaluation = self.minimax(temp_board, True, depth - 1, alpha, beta)[0]
+                evaluation = self.minimax_alpha_beta(temp_board, True, depth - 1, alpha, beta)[0]
                 if evaluation < min_eval:
                     min_eval = evaluation
                     best_move = (row, col)
@@ -100,6 +156,8 @@ class AI:
         row, col = random.choice(emptyPositions)
 
         return [row, col]
+    def reset_node_count(self):
+        self.node_count = 0
 
 
 def score_position(board, piece):
---
+++ performance_measurement.py
@@ -0,0 +1,68 @@
+import time
+import matplotlib.pyplot as plt
+from ai import AI
+from board import Board
+
+ai = AI()
+main_board = Board()
+
+def measure_execution_time_minmax(board, algorithm):
+    start_time = time.time()
+    algorithm(board, maximizing=True, depth=3)
+    end_time = time.time()
+    return end_time - start_time
+
+def measure_execution_time_alph_beta(board, algorithm):
+    start_time = time.time()
+    algorithm(board, maximizing=True, depth=3, alpha=-100, beta=100)
+    end_time = time.time()
+    return end_time - start_time
+# Number of nodes expanded
+def count_nodes_expanded_alpha_beta(board, algorithm):
+    algorithm(board, maximizing=True, depth=3, alpha=-100, beta=100)
+    return ai.node_count
+
+def count_nodes_expanded_minimax(board, algorithm):
+    algorithm(board, maximizing=True, depth=3)
+    return ai.node_count
+
+# Measure execution time
+execution_time_minimax = measure_execution_time_minmax(main_board, ai.minimax)
+execution_time_minimax_alpha_beta = measure_execution_time_alph_beta(main_board, ai.minimax_alpha_beta)
+
+# Count nodes expanded
+ai.reset_node_count()
+nodes_expanded_minimax = count_nodes_expanded_minimax(main_board, ai.minimax)
+ai.reset_node_count()
+nodes_expanded_minimax_alpha_beta = count_nodes_expanded_alpha_beta(main_board, ai.minimax_alpha_beta)
+
+# Plot the results in one figure
+labels = ['Minimax', 'Minimax with Alpha-Beta Pruning']
+execution_times = [execution_time_minimax, execution_time_minimax_alpha_beta]
+nodes_expanded = [nodes_expanded_minimax, nodes_expanded_minimax_alpha_beta]
+
+x = range(len(labels))
+
+# Create subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+
+# Plot execution time
+ax1.bar(x, execution_times, alpha=0.8)
+ax1.set_ylabel('Execution Time')
+ax1.set_xticks(x)
+ax1.set_xticklabels(labels)
+ax1.set_title('Execution Time Comparison')
+
+# Plot nodes expanded
+ax2.bar(x, nodes_expanded, alpha=0.8)
+ax2.set_xlabel('Algorithm')
+ax2.set_ylabel('Nodes Expanded')
+ax2.set_xticks(x)
+ax2.set_xticklabels(labels)
+ax2.set_title('Nodes Expanded Comparison')
+
+# Adjust spacing between subplots
+plt.tight_layout()
+
+# Display the combined figure
+plt.show()
